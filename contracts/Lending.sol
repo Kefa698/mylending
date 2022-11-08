c9990fa391205cfabc8ad7fc0744404a1685118d
Kefa698 <kefaombongi07@gmail.com>
2022-11-08 15:55:39 +0300

    variables and mappings

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Lending {
-    constructor() {}
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Lending is ReentrancyGuard, Ownable {
+    mapping(address => address) public s_tokenToPricefeed;
+    address[] public s_allowedTokens;
+
+    //account   ->token   ->amount
+    mapping(address => mapping(address => uint256)) public s_accountToTokenDeposits;
+
+    //account   ->token   ->amount
+    mapping(address => mapping(address => uint256)) public s_accountToTokenBorrows;
+
+    // 5% Liquidation Reward
+    uint256 public constant LIQUIDATION_REWARD = 5;
+    // At 80% Loan to Value Ratio, the loan can be liquidated
+    uint256 public constant LIQUIDATION_THRESHOLD = 80;
+    uint256 public constant MIN_HEALH_FACTOR = 1e18;
 }
