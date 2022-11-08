774190a31458322d84992aa3173ced998d71319f
Kefa698 <kefaombongi07@gmail.com>
2022-11-08 14:58:17 +0300

    first commit

---
+++ contracts/RewardToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RewardToken is ERC20 {
+    constructor() ERC20("RewardToken", "RT") {
+        _mint(msg.sender, 1000000 * 10**18);
+    }
+}
